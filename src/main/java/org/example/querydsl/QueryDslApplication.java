42089ce18ccd6cde59eb8c5047f3f83782103843
JJ Lee <maiload77@gmail.com>
2024-06-22 15:17:38 +0900

    First commit

---
+++ src/main/java/org/example/querydsl/QueryDslApplication.java
@@ -0,0 +1,13 @@
+package org.example.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryDslApplication.class, args);
+    }
+
+}
